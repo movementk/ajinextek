5af4f8f3bdcfaf79d93c0e56c1d01e06e0cb6ecd
kimyh5392 <mk.kimyh@gmail.com>
2018-10-12 14:44:15 +0900

    웹 채용정보 작업중 . . .

--- careers/careers_info.php
+++ careers/careers_info.php
@@ -19,12 +19,77 @@
     </div>
     
     <!-- Content Start -->
-    <main id="content">
-        <div class="container">
-            <section class="careers-info">
+    <main id="content" class="careers-info">
+        <section class="capable">
+            <div class="container">
+                <div class="section-header">
+                    <h3>인재상</h3>
+                    <p>아진엑스텍은 다음과 같은 인재를 기다립니다</p>
+                </div>
+                <div class="section-content">
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/careers/img_capable01.png" alt="창조">
+                                <figcaption>
+                                    <dl>
+                                        <dt>창조</dt>
+                                        <dd>창조적이며 도전적으로<br>비젼을 성취하는 자</dd>
+                                    </dl>
+                                </figcaption>
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/careers/img_capable02.png" alt="도전">
+                                <figcaption>
+                                    <dl>
+                                        <dt>도전</dt>
+                                        <dd>진취적인 자세로 끊임없이<br>자기 계발을 하는자 </dd>
+                                    </dl>
+                                </figcaption>
+                            </figure>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/careers/img_capable03.png" alt="변화">
+                                <figcaption>
+                                    <dl>
+                                        <dt>변화</dt>
+                                        <dd>변화와 미래를 선도하는<br> 글로벌 역량을 가진자</dd>
+                                    </dl>
+                                </figcaption>
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <article class="recruitment">
+            <div class="container">
+                <div class="article-header">
+                    <h4>1</h4>
+                </div>
+                <div class="article-content">
+                    
+                </div>
+            </div>
+        </article>
+        <article class="personnel">
+            <div class="container">
+                <div class="article-header">
+                    <h4>2</h4>
+                </div>
+                <div class="article-content">
+                    
+                </div>
+            </div>
+        </article>
+        <article class="welfare">
+            <div class="container">
                 
-            </section>
-        </div>
+            </div>
+        </article>
     </main>
     
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php');?>
